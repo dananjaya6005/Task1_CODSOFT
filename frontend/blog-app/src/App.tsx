efa3c6e3d0920346d9def84d36f33b73a8dae723
dananjaya6005 <dananjaya6005@gmail.com>
2023-12-20 10:25:35 +0530

    fix deploy netlify

--- frontend/blog-app/src/App.tsx
+++ frontend/blog-app/src/App.tsx
@@ -2,8 +2,6 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter} from "react-router-dom";
 import MainRouter from "../MainRouter";
 
-
-
 const clerkPubKey = "pk_test_dG91Y2hlZC1ndWxsLTI2LmNsZXJrLmFjY291bnRzLmRldiQ";
 
 export default function App() {
